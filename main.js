9238ab0de38d89c5eae8bc04b9303382a3a41843
Leigh <leighbryant@bellsouth.net>
2016-10-01 13:59:33 -0400

    Added icons and basic animal info

---
+++ main.js
@@ -0,0 +1,20 @@
+// fennecFox
+// atlanticPuffin
+
+//test as prototype method
+
+function Animal(name, sex, dateOfBirth, birthAbility, habitat){
+  this.stats = {
+    name: name,
+    sex: sex,
+    dateOfBirth: dateOfBirth,
+    birthAbility: birthAbility,
+    habitat: habitat,
+  };
+}
+
+var fennecFox = new Animal('Franz', 'Female', 'dateOfBirth', 'Able', 'Desert');
+// fennecFox.init();
+var atlanticPuffin = new Animal('Pablo', 'Male', 'dateOfBirth', 'Unable', 'Beach/Ocean');
+console.log(fennecFox);
+console.log(atlanticPuffin);
