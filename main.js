6baeb4f7831fc94fc981058700d770528dafb9f7
Leigh <leighbryant@bellsouth.net>
2016-10-02 21:36:44 -0400

    fixed code and worked on methods:

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
-// fennecFox
+// fennecFox //'Franz', 'Female', 'dateOfBirth', 'Able', 'Desert'
 // atlanticPuffin
+//    sex: sex,
+//    habitat: habitat,
 
 //test as prototype method
+Animal.prototype = {
+    birthAbility: function() {
+        var animal = this.name;
+        if ('Fennec Fox' === animal) {
+            return 'This is a mammal.';
+        } else {
+            return 'This is a bird';
+        }
+    },
 
-function Animal(name, sex, dateOfBirth, birthAbility, habitat){
-  this.stats = {
-    name: name,
-    sex: sex,
-    dateOfBirth: dateOfBirth,
-    birthAbility: birthAbility,
-    habitat: habitat,
-  };
+    dateOfBirth: function(age) {
+        var currentYear = new Date().getFullYear();
+        var birthYear = this.age.split("/")[2];
+        console.log(currentYear - birthYear);
+    }
+
 }
 
-var fennecFox = new Animal('Franz', 'Female', 'dateOfBirth', 'Able', 'Desert');
-// fennecFox.init();
-var atlanticPuffin = new Animal('Pablo', 'Male', 'dateOfBirth', 'Unable', 'Beach/Ocean');
-console.log(fennecFox.stats);
-console.log(atlanticPuffin.stats);
+function Animal(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+var fennecFox = new Animal('Fennec Fox', '09/07/1999');
+console.log(fennecFox.birthAbility());
+console.log(fennecFox.dateOfBirth());
+
+
+var atlanticPuffin = new Animal('Atlantic Puffin', '06/09/2010');
+console.log(atlanticPuffin.birthAbility());
+console.log(atlanticPuffin.dateOfBirth());
+
+
+fennecFox.speak = function(line){
+  console.log ("My name is Franz and I am" + atlanticPuffin.dateOfBirth()+ "years old.");
+}
+
+atlanticPuffin.speak = function(line){
+  console.log
+}
+
+
+// console.log(fennecFox);
+// console.log(atlanticPuffin);
