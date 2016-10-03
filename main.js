d29ab3dd034194f82b3b093b93cec5c698829819
Leigh <leighbryant@bellsouth.net>
2016-10-03 15:21:44 -0400

    Created working animals and HTML

--- main.js
+++ main.js
@@ -3,48 +3,52 @@
 //    sex: sex,
 //    habitat: habitat,
 
-//test as prototype method
 Animal.prototype = {
     birthAbility: function() {
         var animal = this.name;
-        if ('Fennec Fox' === animal) {
-            return 'This is a mammal.';
+        if ('Franz' === animal) {
+            return 'mammal';
         } else {
-            return 'This is a bird';
+            return 'bird';
         }
     },
 
     dateOfBirth: function(age) {
         var currentYear = new Date().getFullYear();
         var birthYear = this.age.split("/")[2];
-        console.log(currentYear - birthYear);
+        return (currentYear - birthYear);
+    },
+
+    nameIs: function() {
+        var name = this.name;
+        return name;
+    },
+
+    habitatIs: function() {
+        var habitat = this.habitat;
+        return habitat;
+    },
+
+    dietIs: function() {
+      var diet = this.diet;
+      return diet;
+    },
+
+    speak: function() {
+        return ("My name is " + this.nameIs() + " and I am " + this.dateOfBirth() + " years old. I am a " + this.birthAbility() + ". I live in the " + this.habitatIs() + ". I love to eat " + this.dietIs() + ".");
     }
+};
 
-}
-
-function Animal(name, age) {
+function Animal(name, age, habitat, diet) {
     this.name = name;
     this.age = age;
+    this.habitat = habitat;
+    this.diet = diet;
 }
 
-var fennecFox = new Animal('Fennec Fox', '09/07/1999');
-console.log(fennecFox.birthAbility());
-console.log(fennecFox.dateOfBirth());
+var fennecFox = new Animal('Franz', '09/09/1999', 'desert', 'rodents, eggs, reptiles, and insects');
+document.getElementById('foxInfo').innerHTML = fennecFox.speak();
 
 
-var atlanticPuffin = new Animal('Atlantic Puffin', '06/09/2010');
-console.log(atlanticPuffin.birthAbility());
-console.log(atlanticPuffin.dateOfBirth());
-
-
-fennecFox.speak = function(line){
-  console.log ("My name is Franz and I am" + atlanticPuffin.dateOfBirth()+ "years old.");
-}
-
-atlanticPuffin.speak = function(line){
-  console.log
-}
-
-
-// console.log(fennecFox);
-// console.log(atlanticPuffin);
+var atlanticPuffin = new Animal('Pablo', '08/09/2010', 'ocean', 'little fish and sometimes crustaceans');
+document.getElementById('puffinInfo').innerHTML = atlanticPuffin.speak();
