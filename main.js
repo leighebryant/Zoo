31ca26757326da4b657eba4892a7dfd39150b63e
Leigh <leighbryant@bellsouth.net>
2016-10-04 09:23:53 -0400

    Got first asynchronous request to work

--- main.js
+++ main.js
@@ -46,8 +46,7 @@ function Animal(name, age, habitat, diet) {
     this.diet = diet;
 }
 
-var fennecFox = new Animal('Franz', '09/09/1999', 'desert', 'rodents, eggs, reptiles, and insects');
-document.getElementById('foxInfo').innerHTML = fennecFox.speak();
+
 //Testing
 // console.assert(fennecFox.name === "Franz");
 // console.assert(fennecFox.age === "09/09/1999");
@@ -55,18 +54,52 @@ document.getElementById('foxInfo').innerHTML = fennecFox.speak();
 // console.assert(fennecFox.diet === "rodents, eggs, reptiles, and insects");
 
 
-var atlanticPuffin = new Animal('Pablo', '08/09/2010', 'ocean', 'little fish and sometimes crustaceans');
-document.getElementById('puffinInfo').innerHTML = atlanticPuffin.speak();
+// var atlanticPuffin = new Animal('Pablo', '08/09/2010', 'ocean', 'little fish and sometimes crustaceans');
+// document.getElementById('puffinInfo').innerHTML = atlanticPuffin.speak();
 //Testing
 // console.assert(atlanticPuffin.name === "Pablo");
 // console.assert(atlanticPuffin.age === "08/09/2010");
 // console.assert(atlanticPuffin.habitat === "ocean");
 // console.assert(atlanticPuffin.diet === "little fish and sometimes crustaceans");
 
-var capybara = new Animal('Carl', '01/02/2013', 'forest near water', 'grass and water plants');
-document.getElementById('capybaraInfo').innerHTML = capybara.speak();
+// var capybara = new Animal('Carl', '01/02/2013', 'forest near water', 'grass and water plants');
+// document.getElementById('capybaraInfo').innerHTML = capybara.speak();
 //Testing
 // console.assert(capybara.name === "Carl");
 // console.assert(capybara.age === "01/02/2013");
 // console.assert(capybara.habitat === "forest near water");
 // console.assert(capybara.diet === "grass and water plants");
+
+
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    var http = new XMLHttpRequest();
+    http.open('GET', url, true);
+    http.onload = function() {
+      if (http.status === 200) {
+        resolve(JSON.parse(http.response));
+      } else {
+        reject(http.statusText);
+      }
+    };
+    http.onerror = function() {
+      reject(http.statusText);
+    };
+    http.send();
+  });
+}
+
+get('animals.json').then(function(animal) {
+  var fennecFox = new Animal(animal.animals[0].name, animal.animals[0].age, animal.animals[0].habitat, animal.animals[0].diet, animal.animals[0].diet);
+  document.getElementById('foxInfo').innerHTML = fennecFox.speak();
+});
+then(function(animal) {
+    var atlanticPuffin = new Animal(animal.animals[1].name, animal.animals[1].age, animal.animals[1].habitat, animal.animals[1].diet, animal.animals[1].diet);
+    document.getElementById('puffinInfo').innerHTML = atlanticPuffin.speak();
+  });
+then(function(animal) {
+    var capybara = new Animal(animal.animals[2].name, animal.animals[2].age, animal.animals[2].habitat, animal.animals[2].diet, animal.animals[2].diet);
+    document.getElementById('capybaraInfo').innerHTML = capybara.speak();
+}).catch(function(error) {
+  console.log(error);
+});
