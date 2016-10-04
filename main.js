56ed9c247a66d438f4dc3b97f2ebd0e6063a1cf1
Leigh <leighbryant@bellsouth.net>
2016-10-04 15:13:56 -0400

    Working promise JS and JSON

--- main.js
+++ main.js
@@ -24,6 +24,11 @@ Animal.prototype = {
         return name;
     },
 
+    speciesIs: function() {
+      var species = this.species;
+      return species;
+    },
+
     habitatIs: function() {
         var habitat = this.habitat;
         return habitat;
@@ -35,15 +40,16 @@ Animal.prototype = {
     },
 
     speak: function() {
-        return ("My name is " + this.nameIs() + " and I am " + this.dateOfBirth() + " years old. I am a " + this.birthAbility() + ". I live in the " + this.habitatIs() + ". I love to eat " + this.dietIs() + ".");
+        return ("My name is " + this.nameIs() + " the " + this.speciesIs() + " and I am " + this.dateOfBirth() + " years old. I am a " + this.birthAbility() + ". I live in the " + this.habitatIs() + ". I love to eat " + this.dietIs() + ".");
     }
 };
 
-function Animal(name, age, habitat, diet) {
+function Animal(name, age, habitat, diet, species) {
     this.name = name;
     this.age = age;
     this.habitat = habitat;
     this.diet = diet;
+    this.species = species;
 }
 
 
@@ -52,23 +58,22 @@ function Animal(name, age, habitat, diet) {
 // console.assert(fennecFox.age === "09/09/1999");
 // console.assert(fennecFox.habitat === "desert");
 // console.assert(fennecFox.diet === "rodents, eggs, reptiles, and insects");
+// console.assert(fennecFox.species === "Fennec Fox");
 
 
-// var atlanticPuffin = new Animal('Pablo', '08/09/2010', 'ocean', 'little fish and sometimes crustaceans');
-// document.getElementById('puffinInfo').innerHTML = atlanticPuffin.speak();
 //Testing
 // console.assert(atlanticPuffin.name === "Pablo");
 // console.assert(atlanticPuffin.age === "08/09/2010");
 // console.assert(atlanticPuffin.habitat === "ocean");
 // console.assert(atlanticPuffin.diet === "little fish and sometimes crustaceans");
+// console.assert(atlanticPuffin.species === "Atlantic Puffin");
 
-// var capybara = new Animal('Carl', '01/02/2013', 'forest near water', 'grass and water plants');
-// document.getElementById('capybaraInfo').innerHTML = capybara.speak();
 //Testing
 // console.assert(capybara.name === "Carl");
 // console.assert(capybara.age === "01/02/2013");
 // console.assert(capybara.habitat === "forest near water");
 // console.assert(capybara.diet === "grass and water plants");
+// console.assert(capybara.species === "Capybara");
 
 
 function get(url) {
@@ -89,17 +94,20 @@ function get(url) {
   });
 }
 
-get('animals.json').then(function(animal) {
-  var fennecFox = new Animal(animal.animals[0].name, animal.animals[0].age, animal.animals[0].habitat, animal.animals[0].diet, animal.animals[0].diet);
+var promise = $.get('animals.json');
+
+promise.then(function(animal) {
+  var fennecFox = new Animal(animal.animals[0].name, animal.animals[0].age, animal.animals[0].habitat, animal.animals[0].diet, animal.animals[0].species);
   document.getElementById('foxInfo').innerHTML = fennecFox.speak();
-});
-then(function(animal) {
-    var atlanticPuffin = new Animal(animal.animals[1].name, animal.animals[1].age, animal.animals[1].habitat, animal.animals[1].diet, animal.animals[1].diet);
+  return promise;
+}).then(function(animal) {
+    var atlanticPuffin = new Animal(animal.animals[1].name, animal.animals[1].age, animal.animals[1].habitat, animal.animals[1].diet, animal.animals[1].species);
     document.getElementById('puffinInfo').innerHTML = atlanticPuffin.speak();
-  });
-then(function(animal) {
-    var capybara = new Animal(animal.animals[2].name, animal.animals[2].age, animal.animals[2].habitat, animal.animals[2].diet, animal.animals[2].diet);
+    return promise;
+  }).then(function(animal) {
+    var capybara = new Animal(animal.animals[2].name, animal.animals[2].age, animal.animals[2].habitat, animal.animals[2].diet, animal.animals[2].species);
     document.getElementById('capybaraInfo').innerHTML = capybara.speak();
+    return promise;
 }).catch(function(error) {
   console.log(error);
 });
